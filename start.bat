b2f2f1c41a97055a9b1e2a7ee265b6cacfb3d9ef
Si-feizhai <36579811+Aizizhu@users.noreply.github.com>
2025-07-11 12:37:03 +0800

    start.bat

--- start.bat
+++ start.bat
@@ -8,9 +8,10 @@ cd /d %~dp0
 
 set /p start_url=请输入起始页链接（如 https://xc8866.com/forum-23-1.htm?tagids=151_0_0_0）:
 set /p total_pages=请输入总共爬取页数（数字）:
+set /p max_workers=请输入同时爬取的最大进程数（默认6，建议不要太大）:
 
 call venv\Scripts\activate
 
-python main.py --start-url %start_url% --total-pages %total_pages%
+python main.py --start-url %start_url% --total-pages %total_pages% --max-workers %max_workers%
 
 pause
